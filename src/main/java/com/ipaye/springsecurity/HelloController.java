b35a4de6416292ae5b99cb83ed15af9a85a9ff23
Ipaye Babatunde <fineboyhakim@gmail.com>
2024-12-28 23:54:41 +0100

    init commit

---
+++ src/main/java/com/ipaye/springsecurity/HelloController.java
@@ -0,0 +1,14 @@
+package com.ipaye.springsecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String Hello(HttpServletRequest request){
+        return "Welcome to Insta " + request.getSession().getId();
+    }
+}
